8c0ee7019050adffa061891d83301df6b36d58fb
DannyVC123 <dannyjhuo@gmail.com>
2023-02-27 12:40:34 -0600

    fixing arduino bluetooth code

--- arduino code/HM10_Test/HM10_Test.ino
+++ arduino code/HM10_Test/HM10_Test.ino
@@ -1,20 +1,24 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial HM10(2, 3); // RX = 2, TX = 3
-
-char appData;  
-String inData = "";
+SoftwareSerial bluetooth(10, 11);
 
 void setup() {
 
   Serial.begin(9600);
   Serial.println("HM10 serial started at 9600");
-  HM10.begin(9600); // set HM10 serial at 9600 baud rate
+  bluetooth.begin(9600); // set HM10 serial at 9600 baud rate
   pinMode(13, OUTPUT); // onboard LED
   digitalWrite(13, LOW); // switch OFF LED
 }
 
 void loop() {
+  if (bluetooth.available()) {
+    char recieved = bluetooth.read();
+    
+    Serial.print("Recieved data: ");
+    Serial.println(recieved);
+  }
+  /*
   HM10.listen();  // listen the HM10 port
   while (HM10.available() > 0) {   // if HM10 sends something then read
     appData = HM10.read();
@@ -27,17 +31,6 @@ void loop() {
     HM10.write(Serial.read());
   }
 
-  if ( inData == "F") {
-    Serial.println("LED OFF");
-    digitalWrite(13, LOW); // switch OFF LED
-    delay(500);
-  }
-
-  if ( inData == "N") {
-    Serial.println("LED ON");
-    digitalWrite(13, HIGH); // switch ON LED
-    delay(500);
-    digitalWrite(13, LOW); // switch OFF LED
-    delay(500);
-  }
+  Serial.print(inData);
+  */
 }
